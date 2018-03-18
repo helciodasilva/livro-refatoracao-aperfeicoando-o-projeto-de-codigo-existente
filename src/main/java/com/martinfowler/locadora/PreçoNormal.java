1b9adc319b2d160a9654c1fdde9a27607328b3cc
Helcio da Silva <helciodasilva@gmail.com>
2018-03-18 11:15:29 -0300

    Capítulo 1 - Exemplo 16

--- src/main/java/com/martinfowler/locadora/PreçoNormal.java
+++ src/main/java/com/martinfowler/locadora/PreçoNormal.java
@@ -4,4 +4,11 @@ class PreçoNormal extends Preço {
 	int lerCódigoPreço() {
 		return Filme.NORMAL;
 	}
+
+	double lerPreço(int diasAlugados) {
+		double resultado = 2;
+		if (diasAlugados > 2)
+			resultado += (diasAlugados - 2) * 1.5;
+		return resultado;
+	}
 }
