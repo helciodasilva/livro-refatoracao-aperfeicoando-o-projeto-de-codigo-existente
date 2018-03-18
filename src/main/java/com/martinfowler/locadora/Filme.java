8627b0ad1277a95bd29d609c27f26ab49ffb1edf
Helcio da Silva <helciodasilva@gmail.com>
2018-03-18 11:01:39 -0300

    Capítulo 1 - Exemplo 12

--- src/main/java/com/martinfowler/locadora/Filme.java
+++ src/main/java/com/martinfowler/locadora/Filme.java
@@ -4,7 +4,7 @@ public class Filme {
 	public static final int INFANTIL = 2;
 	public static final int NORMAL = 0;
 	public static final int LANÇAMENTO_NOVO = 1;
-	
+
 	private String _título;
 	private int _codigoPreço;
 
@@ -23,5 +23,26 @@ public class Filme {
 
 	public String lerTítulo() {
 		return _título;
-	};
+	}
+
+	double lerPreço(int diasAlugados) {
+		double resultado = 0;
+		switch (lerCódigoPreço()) {
+		case Filme.NORMAL:
+			resultado += 2;
+			if (diasAlugados > 2)
+				resultado += (diasAlugados - 2) * 1.5;
+			break;
+		case Filme.LANÇAMENTO_NOVO:
+			resultado += diasAlugados * 3;
+			break;
+		case Filme.INFANTIL:
+			resultado += 1.5;
+			if (diasAlugados > 3)
+				resultado += (diasAlugados - 3) * 1.5;
+			break;
+		}
+		return resultado;
+	}
+
 }
