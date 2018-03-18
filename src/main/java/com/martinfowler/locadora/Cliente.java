7d1ee547e3f05d99115186e98a1f93fb66d7674f
Helcio da Silva <helciodasilva@gmail.com>
2018-03-18 09:38:13 -0300

    Capítulo 1 - Exemplo 2

--- src/main/java/com/martinfowler/locadora/Cliente.java
+++ src/main/java/com/martinfowler/locadora/Cliente.java
@@ -27,30 +27,15 @@ class Cliente {
 		while (locações.hasMoreElements()) {
 			double estaQuantia = 0;
 			Locação cada = (Locação) locações.nextElement();
-			
-			//determinar quantias para cada linha
-			switch (cada.lerFilme().lerCódigoPreço()) {
-			case Filme.NORMAL:
-				estaQuantia += 2;
-				if (cada.lerDiasAlugados() > 2)
-					estaQuantia += (cada.lerDiasAlugados() - 2) * 1.5;
-				break;
-			case Filme.LANÇAMENTO_NOVO:
-				estaQuantia += cada.lerDiasAlugados() * 3;
-				break;
-			case Filme.INFANTIL:
-				estaQuantia += 1.5;
-				if (cada.lerDiasAlugados() > 3)
-					estaQuantia += (cada.lerDiasAlugados() - 3) * 1.5;
-				break;
-			}
-			
+
+			estaQuantia = quantiaDe(cada);
+
 			//adicionar os pontos do locador freqüente
 			pontosLocadorFreqüente++;
 			//adicionar bônus para uma locação de lançamentos por dois dias
 			if ((cada.lerFilme().lerCódigoPreço() == Filme.LANÇAMENTO_NOVO) && cada.lerDiasAlugados() > 1)
 				pontosLocadorFreqüente++;
-			
+
 			//mostrar valores para esta locação
 			resultado += "\t" + cada.lerFilme().lerTítulo() + "\t" + String.valueOf(estaQuantia) + "\n";
 			quantiaTotal += estaQuantia;
@@ -59,6 +44,27 @@ class Cliente {
 		resultado += "O valor devido é " + String.valueOf(quantiaTotal) + "\n";
 		resultado += "Você ganhou " + String.valueOf(pontosLocadorFreqüente) + " pontos de locador freqüente";
 		return resultado;
+	}
+
+	private int quantiaDe(Locação cada) {
+		int estaQuantia = 0;
+		//determinar quantias para cada linha
+		switch (cada.lerFilme().lerCódigoPreço()) {
+		case Filme.NORMAL:
+			estaQuantia += 2;
+			if (cada.lerDiasAlugados() > 2)
+				estaQuantia += (cada.lerDiasAlugados() - 2) * 1.5;
+			break;
+		case Filme.LANÇAMENTO_NOVO:
+			estaQuantia += cada.lerDiasAlugados() * 3;
+			break;
+		case Filme.INFANTIL:
+			estaQuantia += 1.5;
+			if (cada.lerDiasAlugados() > 3)
+				estaQuantia += (cada.lerDiasAlugados() - 3) * 1.5;
+			break;
+		}
+		return estaQuantia;
 	}
 
 }
