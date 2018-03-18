25c5862d433be9f1d6a7e9b4e858994b72e3f106
Helcio da Silva <helciodasilva@gmail.com>
2018-03-18 11:12:06 -0300

    Capítulo 1 - Exemplo 15

--- src/main/java/com/martinfowler/locadora/Filme.java
+++ src/main/java/com/martinfowler/locadora/Filme.java
@@ -38,23 +38,7 @@ public class Filme {
 	}
 
 	double lerPreço(int diasAlugados) {
-		double resultado = 0;
-		switch (lerCódigoPreço()) {
-		case Filme.NORMAL:
-			resultado += 2;
-			if (diasAlugados > 2)
-				resultado += (diasAlugados - 2) * 1.5;
-			break;
-		case Filme.LANÇAMENTO_NOVO:
-			resultado += diasAlugados * 3;
-			break;
-		case Filme.INFANTIL:
-			resultado += 1.5;
-			if (diasAlugados > 3)
-				resultado += (diasAlugados - 3) * 1.5;
-			break;
-		}
-		return resultado;
+		return _preço.lerPreço(diasAlugados);
 	}
 
 	int lerPontosLocadorFreqüente(int diasAlugados) {
--- src/main/java/com/martinfowler/locadora/Preço.java
+++ src/main/java/com/martinfowler/locadora/Preço.java
@@ -2,4 +2,24 @@ package com.martinfowler.locadora;
 
 abstract class Preço {
 	abstract int lerCódigoPreço();
+
+	double lerPreço(int diasAlugados) {
+		double resultado = 0;
+		switch (lerCódigoPreço()) {
+		case Filme.NORMAL:
+			resultado += 2;
+			if (diasAlugados > 2)
+				resultado += (diasAlugados - 2) * 1.5;
+			break;
+		case Filme.LANÇAMENTO_NOVO:
+			resultado += diasAlugados * 3;
+			break;
+		case Filme.INFANTIL:
+			resultado += 1.5;
+			if (diasAlugados > 3)
+				resultado += (diasAlugados - 3) * 1.5;
+			break;
+		}
+		return resultado;
+	}
 }
